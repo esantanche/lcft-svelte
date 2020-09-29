f2f582fcbab4a05c1691630327cce614ff5d0982
Emanuele Santanche <esantanche@gmail.com>
2020-09-29 11:45:41 +0200

    Working on google analytics tracking code

--- sapperapp/src/routes/_layout.svelte
+++ sapperapp/src/routes/_layout.svelte
@@ -7,15 +7,19 @@ help search engines to index the item's page.
 <script>
     import StandardFooter from "../components/footers/StandardFooter.svelte";
     import StandardHeader from "../components/headers/StandardHeader.svelte";
+    import GoogleAnalytics from "../components/trackers/GoogleAnalytics.svelte";
 
     // The absence of the prop segment triggers this warning: "<Layout> was created with unknown prop 'segment'"
     // But here I don't need this prop and if I define it, I get an error
     // export let segment;
+
 </script>
 
 <svelte:head>
     <meta property="google-site-verification" content="PMDPQpQDqCmyGM4HeruuOXemu1X9UxBcU9qop3ywqOQ" />
 </svelte:head>
+
+<GoogleAnalytics />
 
 <StandardHeader />
 
